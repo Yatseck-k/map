21266bf7652d986a137c6da36281abdb5546e77b
Yatseck <34891982+Yatseck-k@users.noreply.github.com>
2021-08-26 22:48:33 +0300

    обработка массива вывод точки на карте

--- index.php
+++ index.php
@@ -1,8 +1,24 @@
 <?php
 $data = file_get_contents('http://www.marsruty.ru/krasnodar/gps.txt');
+if (!$data) {
+    echo 'Данные недоступны';
+    exit();
+}
 $keys = array('type', 'number', 'dolgota', 'shirota', 'speed', 'angle', 'bortNumber', 'break');
 
 $data = explode(chr(10), $data);
+//эксперемент перечисления массивов
+$troll[] = null;
+$bus[] = null;
+$tram[] = null;
+
+$typeOT = array(
+    0 => $troll,
+    1 => $bus,
+    2 => $tram,
+);
+$idType = 0;
+
 foreach ($data as $ot => $value) { // разбиваем по типу ОТ
     // echo $ot . ' ' . $value . PHP_EOL;
     if (substr($value, 0) == 1) {
@@ -15,25 +31,54 @@ foreach ($data as $ot => $value) { // разбиваем по типу ОТ
         $tram[] = $value;
     }
 }
+//foreach ($typeOT as $anyOne) {
 foreach ($troll as $id => $value) {
     $oneTroll[] = explode(',', $value);
 
+
 }
-$count = count($oneTroll);
-$count = $count -1;
-for ($i = 0; $i == 35; $i++) {
-   // $newTroll = array_combine($keys, $oneTroll[$idTroll]);
-    echo $i;
+$idTroll = 0;
+foreach ($oneTroll as $anyTwo => $value) {
+    if (!$oneTroll) {
+        continue;
+    }
+    $newTroll = array_combine($keys, $oneTroll[$idTroll]);
+
+    $newTroll['dolgota'] = substr_replace($newTroll['dolgota'], '.', 2, 0);
+    $newTroll['shirota'] = substr_replace($newTroll['shirota'], '.', 2, 0);
+    $position = $newTroll['shirota'] . ',' . $newTroll['dolgota'];
+        $idTroll++;
 }
 
+?>
 
-//echo count($keys) . PHP_EOL;
-//echo count($oneTroll[]) . PHP_EOL;
+<div id="map" style="width: 100%; height:500px"></div>
 
-//$objectTroll = (object)$oneTroll;
-//print_r($keys);
-//print_r($bus). PHP_EOL;
-//print_r($tram). PHP_EOL;
+<script src="https://api-maps.yandex.ru/2.1/?lang=ru-RU" type="text/javascript"></script>
+<script type="text/javascript">
+ymaps.ready(init);
+function init() {
+	var myMap = new ymaps.Map("map", {
+		center: [<?php echo $position; ?>],
+		zoom: 16
+	}, {
+		searchControlProvider: 'yandex#search'
+	});
 
+	var myCollection = new ymaps.GeoObjectCollection();
 
+	// Добавим метку красного цвета.
+	var myPlacemark = new ymaps.Placemark([
+		<?php echo $position; ?>
+	], {
+		balloonContent: '<?php echo $position; ?>'
+	}, {
+		preset: 'islands#icon',
+		iconColor: '#ff0000'
+	});
+	myCollection.add(myPlacemark);
 
+	myMap.geoObjects.add(myCollection);
+}
+</script>
+<div id="map" style="width: 100%; height:500px"></div>
